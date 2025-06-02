799a85f1ad4e0d859ac7f1132a206478d3f3443b
Yusufsahin1 <ys.sahinyusuf@gmail.com>
2025-06-02 20:35:17 +0300

    initial commit

---
+++ backend/src/main/java/com/yusufsahin/iot_platform/IotPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.yusufsahin.iot_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IotPlatformApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/yusufsahin/iot_platform/IotPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yusufsahin.iot_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
